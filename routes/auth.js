d97b043ade05e29090ec36ab032b10225be9cc1d
Akshay Anand <21bcs015@iiitdmj.ac.in>
2024-11-17 18:33:11 +0530

    token expiry time changed

--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,7 @@ router.post('/login', async (req, res) => {
   const validPass = await bcrypt.compare(password, user.password);
   if (!validPass) return res.status(400).send({msg:'Invalid email or password'});
 
-  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
   res.header('Authorization', token).send({ token });
 });
 
